bfa5045d195f58712d42b5ec32f51bbb172e9a85
snowxball <errant1337@gmail.com>
2026-06-16 10:52:58 +0700

    Delete 99FarmDM.lua

--- 99FarmDM.lua
+++
@@ -1,7 +0,0 @@
-getgenv().hop = "old"
-getgenv().reducelag = true
-getgenv().blackscreen = false
-getgenv().AutoFarm = false
-getgenv().autoexecuter = true
-getgenv().WebhookURL = "https://discord.com/api/webhooks/1467375115447832700/ezEqVRAl4QQDL_bhpHOkSn4ptmfflRkHJOOxSS2xhivcEhRDaGQrjmloFEyZfVO5m4Sr" --If not used do not write anything
-loadstring(game:HttpGet("https://raw.githubusercontent.com/caomod2077/Script/refs/heads/main/FoxnameFarmdiamond.lua"))()
